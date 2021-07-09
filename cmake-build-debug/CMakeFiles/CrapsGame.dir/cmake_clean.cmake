9c3a94707f216cb6269ec5c2b3de9077da438b03
BKKajita <bruno.kajita@tutanota.com>
2021-07-09 20:20:22 -0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/CrapsGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CrapsGame.dir/main.c.obj"
+  "CrapsGame.exe"
+  "CrapsGame.exe.manifest"
+  "CrapsGame.pdb"
+  "libCrapsGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CrapsGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
